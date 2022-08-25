404474037e3b1c314e4cb9785e3fe06f7a7c80a0
Tom Close <tom.g.close@gmail.com>
2022-08-26 09:43:20 +1000

    added fname_presurffix function used in apply mask

--- pydra/tasks/nibabel/utils.py
+++ pydra/tasks/nibabel/utils.py
@@ -1,6 +1,8 @@
+from pathlib import Path
 import nibabel as nb
 import numpy as np
 from pydra import mark
+from pydra.engine.specs import File
 
 
 @mark.task
@@ -11,7 +13,7 @@ def apply_mask(in_file: File, in_mask: File, threshold: float = 0.5):
     msknii = nb.load(in_mask)
     msk = msknii.get_fdata() > threshold
 
-    out_file = fname_presuffix(in_file, suffix="_masked", newpath=runtime.cwd)
+    out_file = fname_presuffix(in_file, suffix="_masked", newpath=str(Path.cwd()))
 
     if img.dataobj.shape[:3] != msk.shape:
         raise ValueError("Image and mask sizes do not match.")
@@ -23,6 +25,98 @@ def apply_mask(in_file: File, in_mask: File, threshold: float = 0.5):
         msk = msk[..., np.newaxis]
 
     masked = img.__class__(img.dataobj * msk, None, img.header)
-    masked.to_filename(self._results["out_file"])
+    masked.to_filename(out_file)
 
     return out_file
+
+
+def fname_presuffix(fname, prefix="", suffix="", newpath=None, use_ext=True):
+    """Manipulates path and name of input filename
+
+    Parameters
+    ----------
+    fname : string
+        A filename (may or may not include path)
+    prefix : string
+        Characters to prepend to the filename
+    suffix : string
+        Characters to append to the filename
+    newpath : string
+        Path to replace the path of the input fname
+    use_ext : boolean
+        If True (default), appends the extension of the original file
+        to the output name.
+
+    Returns
+    -------
+    Absolute path of the modified filename
+
+    >>> from nipype.utils.filemanip import fname_presuffix
+    >>> fname = 'foo.nii.gz'
+    >>> fname_presuffix(fname,'pre','post','/tmp')
+    '/tmp/prefoopost.nii.gz'
+
+    >>> from nipype.interfaces.base import Undefined
+    >>> fname_presuffix(fname, 'pre', 'post', Undefined) == \
+            fname_presuffix(fname, 'pre', 'post')
+    True
+
+    """
+    pth, fname, ext = split_filename(fname)
+    if not use_ext:
+        ext = ""
+
+    # No need for isdefined: bool(Undefined) evaluates to False
+    if newpath:
+        pth = op.abspath(newpath)
+    return op.join(pth, prefix + fname + suffix + ext)
+
+
+def split_filename(fname):
+    """Split a filename into parts: path, base filename and extension.
+
+    Parameters
+    ----------
+    fname : str
+        file or path name
+
+    Returns
+    -------
+    pth : str
+        base path from fname
+    fname : str
+        filename from fname, without extension
+    ext : str
+        file extension from fname
+
+    Examples
+    --------
+    >>> from nipype.utils.filemanip import split_filename
+    >>> pth, fname, ext = split_filename('/home/data/subject.nii.gz')
+    >>> pth
+    '/home/data'
+
+    >>> fname
+    'subject'
+
+    >>> ext
+    '.nii.gz'
+
+    """
+
+    special_extensions = [".nii.gz", ".tar.gz", ".niml.dset"]
+
+    pth = op.dirname(fname)
+    fname = op.basename(fname)
+
+    ext = None
+    for special_ext in special_extensions:
+        ext_len = len(special_ext)
+        if (len(fname) > ext_len) and (fname[-ext_len:].lower() == special_ext.lower()):
+            ext = fname[-ext_len:]
+            fname = fname[:-ext_len]
+            break
+    if not ext:
+        fname, ext = op.splitext(fname)
+
+    return pth, fname, ext
