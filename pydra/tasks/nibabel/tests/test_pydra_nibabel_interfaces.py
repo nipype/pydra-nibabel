69914324e15c78d14a1805270835d456691ab59e
Antoine Blachair <antoine.busy@gmail.com>
2022-09-16 12:37:47 +1000

    Add test for exceptions

--- pydra/tasks/nibabel/tests/test_pydra_nibabel_interfaces.py
+++ pydra/tasks/nibabel/tests/test_pydra_nibabel_interfaces.py
@@ -9,13 +9,16 @@ nifti_test_file = cwd + "/data/t1w.nii"
 zeros_mask_file = cwd + "/data/zeros_mask.nii"
 ones_mask_file = cwd + "/data/ones_mask.nii"
 half_zeros_half_ones_mask_file = cwd + "/data/half_zero_ones_mask.nii"
+wrong_shape_mask_file = cwd + "/data/wrong_shape_mask.nii"
+wrong_affine_mask_file = cwd + "/data/wrong_affine_mask.nii"
 
 sorted_array = np.arange(1000).reshape(10, 10, 10)
 ones_mask_array = np.ones((10, 10, 10))
 zeros_mask_array = np.zeros((10, 10, 10))
-m1 = np.zeros((5, 10, 10))
-m2 = np.ones((5, 10, 10))
-half_zeros_half_ones_mask = np.concatenate((m1, m2), axis=0)
+half_zeros_half_ones_mask = np.concatenate(
+    (np.zeros((5, 10, 10)), np.ones((5, 10, 10))), axis=0
+)
+wrong_shape_mask = np.arange(1000).reshape(5, 20, 10)
 
 
 hdr = nb.Nifti1Header()
@@ -55,10 +58,27 @@ nb.save(
     ),
     half_zeros_half_ones_mask_file,
 )
+nb.save(
+    nb.Nifti1Image(
+        wrong_shape_mask,
+        hdr.get_best_affine(),
+        header=hdr,
+    ),
+    wrong_shape_mask_file,
+)
+
+hdr.set_qform(np.diag([2, 4, 6, 2]))
+nb.save(
+    nb.Nifti1Image(
+        ones_mask_array,
+        hdr.get_best_affine(),
+        header=hdr,
+    ),
+    wrong_affine_mask_file,
+)
 
 
 def test_apply_mask_with_ones():
-
     task = apply_mask(in_file=nifti_test_file, in_mask=ones_mask_file)
     result = task()
 
@@ -66,7 +86,6 @@ def test_apply_mask_with_ones():
 
 
 def test_apply_mask_with_zeros():
-
     task = apply_mask(in_file=nifti_test_file, in_mask=zeros_mask_file)
     result = task()
 
@@ -81,3 +100,21 @@ def test_apply_mask_with_half_zeros_half_ones():
 
     assert np.count_nonzero(result_data[0:5]) == 0
     assert np.array_equal(result_data[5:10], nifti_test_file_data[5:10])
+
+
+def test_apply_mask_raises_exception_with_wrong_shape():
+    import pytest
+
+    with pytest.raises(ValueError, match="Image and mask sizes do not match."):
+        task = apply_mask(in_file=nifti_test_file, in_mask=wrong_shape_mask_file)
+        task()
+
+
+def test_apply_mask_raises_exception_with_wrong_affine():
+    import pytest
+
+    with pytest.raises(
+        ValueError, match="Image and mask affines are not similar enough."
+    ):
+        task = apply_mask(in_file=nifti_test_file, in_mask=wrong_affine_mask_file)
+        task()
