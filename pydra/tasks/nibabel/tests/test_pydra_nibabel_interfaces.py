a6c8e9fd7c099b5b5f69a552f62d825799484e71
Tom Close <tom.g.close@gmail.com>
2022-09-30 12:35:07 +1000

    yet again x 2 more debug statements

--- pydra/tasks/nibabel/tests/test_pydra_nibabel_interfaces.py
+++ pydra/tasks/nibabel/tests/test_pydra_nibabel_interfaces.py
@@ -4,8 +4,11 @@ import tempfile
 from pathlib import Path
 import numpy as np
 import pytest
+import sys
 
-raise Exception(f"Nibabel location: {nb.__file__}")
+raise Exception(
+    f"Nibabel location: {nb.__file__}, cwd: {os.getcwd()}, sys.path: {sys.path}"
+)
 from niworkflows.interfaces.nibabel import RegridToZooms
 from pydra.tasks.nibabel.utils import apply_mask, regrid_to_zooms
 
